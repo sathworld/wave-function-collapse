f33b66b466ab4bab952ffc8b2350c43cf207d041
sathworld <48067596+sathworld@users.noreply.github.com>
2023-01-20 12:29:57 -0500

    Add files via upload

---
+++ insert_data.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+#!/bin/bash
+
+# Script to insert data from courses.csv and students.csv into students database
+
+echo $($PSQL "TRUNCATE games, teams;")
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WGOALS OGOALS
+do
+  if [[ $YEAR != "year" ]]
+  then
+    # get winner_id
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER';")
+    # if not found
+    if [[ -z $WINNER_ID ]]
+    then
+      # insert winner
+      INSERT_WINNER_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER');")
+      if [[ $INSERT_WINNER_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted into teams, $WINNER
+      fi
+      # get new winner_id
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER';")
+    fi
+    # get opponent_id
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT';")
+    # if not found
+    if [[ -z $OPPONENT_ID ]]
+    then
+      # insert opponent
+      INSERT_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT');")
+      if [[ $INSERT_OPPONENT_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted into teams, $OPPONENT
+      fi
+      # get new opponent_id
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT';")
+    fi
+    # insert into games
+    INSERT_GAMES_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WGOALS, $OGOALS);")
+    if [[ $INSERT_GAMES_RESULT == "INSERT 0 1" ]]
+    then
+      echo Inserted into games, $YEAR, $ROUND, $WINNER_ID, $OPPONENT_ID, $WGOALS, $OGOALS
+    fi
+  fi
+done
